f8f0ca2da5e9675f3ba7e4e10389e28880d7a699
D.F <mtac@mtac.app>
2023-08-20 22:40:44 -0400

    finish rootless support

--- Classes/ScopeTextView.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface ScopeTextView : UIView
-@property (nonatomic, retain) UITextView *textView;
-@property (nonatomic, retain) UIScrollView *textContainerScrollView;
-- (instancetype)initWithFrame:(CGRect)frame textContainer:(NSTextContainer *)textContainer;
-@end
